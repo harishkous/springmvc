e6027c669d02020574756fb54cdde1d4a358e1f7
Harish <harish@aventyn.com>
2017-01-07 15:13:12 +0530

    changed in sts

--- src/com/ithejas/controller/Helloworld.java
+++ src/com/ithejas/controller/Helloworld.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-//feature second changes
+//feature second changes 
+//feature 2 updated
 @Controller
 public class Helloworld {
 	@RequestMapping("/welcome") 
