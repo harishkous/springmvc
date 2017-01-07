4112a635fa612f913de1bf81efdb238ab86a1e32
harishkous <harishkous@users.noreply.github.com>
2017-01-07 15:04:06 +0530

    Test edited online by Harry

--- src/com/ithejas/spring/config/Config.java
+++ src/com/ithejas/spring/config/Config.java
@@ -22,7 +22,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import com.mongodb.MongoClient;
 
-
+//edited online by Harry
 @Configuration
 //Specifies which package to scan
 @ComponentScan("com.ithejas")
